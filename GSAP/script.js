3c7f9a7ece377998b1603c0c9c7bf1e609605b4b
MalayShikharSoni <malayshikhar2004@gmail.com>
2025-01-30 09:37:08 +0530

    whiskartsss

---
+++ GSAP/script.js
@@ -0,0 +1,6 @@
+gsap.to("#box", {
+    x: 1000,
+    y: 100,
+    duration: 2,
+    delay: 1
+})
---
+++ react/reactlearning/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <form>
+          
+      </form>
+    </div>
+  )
+}
+
+export default App
